7a83d065ea4f20121961a32fc0b6d342db3abb32
umutonigloria <g.umutoni2@alustudent.com>
2024-03-28 15:05:32 -0700

    view leader complaint

--- complaint_system.py
+++ complaint_system.py
@@ -9,8 +9,50 @@ while True:
         print("CITIZEN PORTAL")
 
     elif choice == '*127*300#':
-        print("LEADER ADMIN VIEW PORTAL ")
+        print("LEADER ADMIN VIEW PORTAL")
+        print("1. Admin")
+        print("2. Leader")
+
+        sub_choice = input("Enter portal choice: ")
+
+        if sub_choice == '1':
+            print("Admin Portal")
+            # Add admin functionalities here
+
+        elif sub_choice == '2':
+            print("Leader Portal")
+            print("1. View Complaints")
+
+            leader_choice = input("Enter leader choice: ")
+
+            if leader_choice == '1':
+                print("1. Approved")
+                print("2. Ignored")
+                print("3. Pending")
+
+                complaint_status = input("Enter status choice: ")
+
+                if complaint_status == '1':
+                    print("Displaying approved complaints")
+                    exit(0)
+
+                elif complaint_status == '2':
+                    print("Displaying ignored complaints")
+                    exit(0)
+
+                elif complaint_status == '3':
+                    print("Displaying pending complaints")
+                    exit(0)
+
+                else:
+                    print("Invalid status choice")
+                    exit(0)
+       
+        else:
+            print("Invalid sub-choice")
+            exit(0)
 
     else:
         print("UNKNOWN APPLICATION")
         exit(0)
+
