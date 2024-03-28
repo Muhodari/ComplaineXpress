010dcbcaf80b398d37f8fcaa82476cdd71f075a5
pauline12ish34 <paulineishimwe12@gmail.com>
2024-03-28 14:49:21 -0700

    ft:added option in leader menu

--- complaint_system.py
+++ complaint_system.py
@@ -1,9 +1,15 @@
 # Complaint file
 complaints_file = "complaints-list_1023.txt"
-
+def leadermenu():
+    print("Leader Options:")
+    print("1. View Complaints")
+    print("2. Approve")
+    print("3. Ignore")
+    print("4. Logout")
+    print("5. Exit")
 while True:
 
-    choice = input(" ")
+    choice = input("choice: i")
 
     if choice == '*127#':
         print("CITIZEN PORTAL")
